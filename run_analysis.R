7211caddf0bb4446c337b75d07711212df3724c5
Mike "Xodiv" Battersby <mib@post.com>
2015-12-01 23:27:01 +1100

    fread, reshape2, tryCatch
    Learnt more about data.table. Want to use fread because it is so fast and select= is so useful, but for now not use the j indexing, so set option(datatable.fread.datatable=FALSE) and use fread instead of read.table.
    
    Added a version of the summarising using reshape2 melt/cast to learn them.
    
    Added a tryCatch() around the main code so that I don't have to keep setwd() back manually when I make a mistake developing things.
    
    Also puts the fetch.dataset() call back in.

--- run_analysis.R
+++ run_analysis.R
@@ -2,11 +2,14 @@
 # Tidy the UCI HAR Dataset for Getting and Cleaning Data
 #
 
-# fread is hugely faster than read.table
-library(data.table, warn.conflicts=FALSE)
-
+library(reshape2)
 library(dplyr, warn.conflicts=FALSE)
 library(tidyr)
+
+# fread is hugely faster than read.table, but data.table is quite different from data.frame
+library(data.table, warn.conflicts=FALSE)
+options(datatable.fread.datatable=FALSE)
+
 
 fetch.dataset <- function () {
     
@@ -31,7 +34,7 @@ fetch.dataset <- function () {
 assemble.dataset <- function () {
 
     # Read the features so we can use the observation names, and pull out the mean/std ones.
-    features <- read.table("features.txt", stringsAsFactors=FALSE)
+    features <- fread("features.txt")
     wanted_features <- grep("-(mean|std)\\(\\)", features[,2])
     wanted_names <- features[wanted_features,2]
 
@@ -52,22 +55,35 @@ assemble.dataset <- function () {
 
     # Read the activity labels and use them for factor levels.
     
-    activity_labels <- read.table("activity_labels.txt", col.names=c("id", "label"), stringsAsFactors=FALSE)
+    activity_labels <- fread("activity_labels.txt", col.names=c("id", "label"))
     data$activity <- as.factor(data$activity)
     levels(data$activity) <- activity_labels[,2]
     
     return(data %>% arrange(subject))
 }
 
-create.averaged.dataset <- function(dataset) {
-    dataset %>%
-        group_by(subject, activity) %>%
-        summarize_each(funs(mean)) %>%
-        gather(reading, mean, -subject, -activity)
+create.averaged.dataset <- function(dataset, type='dplyr') {
+    if (type == 'dplyr') {
+        dataset %>%
+            group_by(subject, activity) %>%
+            summarize_each(funs(mean)) %>%
+            gather(reading, mean, -subject, -activity)
+    } else if (type == 'reshape2') {
+        dataset %>%
+            melt(c('subject', 'activity')) %>%
+            dcast(subject + activity ~ variable, fun.aggregate=mean, value=) %>%
+            melt(c('subject', 'activity'))
+    }
 }
 
-#fetch.dataset()
+fetch.dataset()
 oldDir <- setwd("UCI HAR Dataset")
-UCI.HAR.tidy <<- assemble.dataset()
-UCI.HAR.averaged <<- create.averaged.dataset(UCI.HAR.tidy)
-setwd(oldDir)
+tryCatch(
+    {
+        UCI.HAR.tidy <<- assemble.dataset()
+        UCI.HAR.averaged <<- create.averaged.dataset(UCI.HAR.tidy, type='reshape2')
+    },
+    finally = {
+        setwd(oldDir)
+    }
+)
